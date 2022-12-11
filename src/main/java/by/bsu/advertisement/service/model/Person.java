009753fd5de4c356bdb3a9145b1452b037a1d76e
zhenekq <zhenek02ss@gmail.com>
2022-12-11 13:11:45 +0100

    fix: change imports for lower jpa version

--- src/main/java/by/bsu/advertisement/service/model/Person.java
+++ src/main/java/by/bsu/advertisement/service/model/Person.java
@@ -1,10 +1,10 @@
 package by.bsu.advertisement.service.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
