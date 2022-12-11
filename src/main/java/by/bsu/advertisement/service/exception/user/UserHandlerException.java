399545c9731b7fb34d5afacf2e660daf9da8313f
zhenekq <zhenek02ss@gmail.com>
2022-12-11 15:06:07 +0100

    feat: add custom exception related to user logic

---
+++ src/main/java/by/bsu/advertisement/service/exception/user/UserHandlerException.java
@@ -0,0 +1,35 @@
+package by.bsu.advertisement.service.exception.user;
+
+import by.bsu.advertisement.service.exception.ErrorMessage;
+import by.bsu.advertisement.service.exception.device.DeviceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class UserHandlerException {
+
+    @ExceptionHandler(InvalidEmailException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessage invalidEmailException(InvalidEmailException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    public ErrorMessage userAlreadyExistsException(UserAlreadyExistsException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.CONFLICT.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+}
