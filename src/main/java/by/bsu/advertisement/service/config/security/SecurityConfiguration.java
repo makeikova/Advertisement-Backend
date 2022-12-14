71699a689fbe2164faf32c3e6aa62c6c50038ede
zhenekq <zhenek02ss@gmail.com>
2022-12-14 19:40:59 +0100

    Revert "feat: enable swagger endpoints without authentication"
    This reverts commit e34d45fa

--- src/main/java/by/bsu/advertisement/service/config/security/SecurityConfiguration.java
+++ src/main/java/by/bsu/advertisement/service/config/security/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -17,12 +16,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -45,16 +38,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    private static final String[] SWAGGER_ENDPOINTS = {
-            "/v2/api-docs",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**",
-            "/favicon.ico",
-    };
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
@@ -63,14 +46,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.cors().and().authorizeRequests().antMatchers("/api/v1/login/**").permitAll();
-        http.cors().and().authorizeRequests().antMatchers(POST, "/api/v1/user").permitAll();
-        http.cors().and().authorizeRequests().antMatchers("/api/v1/user/refresh").permitAll();
-        http.cors().and().authorizeRequests().antMatchers(SWAGGER_ENDPOINTS).permitAll();
-        http.cors().and().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
-        http.cors().and().authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/api/v1/login/**").permitAll();
+        http.authorizeRequests().antMatchers(POST, "/api/v1/user").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/api/v1/user/refresh").permitAll();
+        http.authorizeRequests().anyRequest().authenticated();
+
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(jwtSecret, jwtAccessTokenExpirationTime, jwtRefreshTokenExpirationTime, authenticationManagerBean());
         CustomAuthorizationFilter customAuthorizationFilter = new CustomAuthorizationFilter(jwtSecret);
 
@@ -84,19 +65,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception{
         return super.authenticationManagerBean();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "x-xsrf-token"));
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
-        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setExposedHeaders(List.of("Authorization"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return source;
     }
 }
